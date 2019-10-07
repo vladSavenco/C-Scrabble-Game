713812e96faeb31ff85199ff85d130c710ffda67
Vlad Savenco <savencov@coventry.ac.uk>
2019-10-07 10:04:46 +0100

    upload 3

--- Vlad_Catalin_Savenco_Scrabble/Vlad_Catalin_Savenco_Scrabble/Vlad_Catalin_Savenco_Scrabble.cpp
+++ Vlad_Catalin_Savenco_Scrabble/Vlad_Catalin_Savenco_Scrabble/Vlad_Catalin_Savenco_Scrabble.cpp
@@ -5,52 +5,135 @@
 #include <string>
 #include <tuple>
 #include <fstream>
+#include <windows.h>
 
 using namespace std;
-
-ifstream File;
 
 bool check(string word)
 {
 	for (std::string::size_type i = 0; i < word.size(); ++i)
 	{
-		if (strchr("abcdefghijklmnopqrstuvwxyz", word[i]) == 0)
+		if (strchr("abcdefghiiklmnopqrstuvwxyz", word[i]))
 		{
-			return false;
 		}
+		else
+			return false;
 	}
 	return true;
 }
 
-bool value(string word)
+int value(string word)
 {
-	return true;
+	int value = 0;
+	for (std::string::size_type i = 0; i < word.size(); ++i)
+	{
+		if (strchr("aeioulnstr", word[i]))
+		{
+			value = value + 1;
+		}
+		if (strchr("dg", word[i]))
+		{
+			value = value + 2;
+		}
+		if (strchr("bcmp", word[i]))
+		{
+			value = value + 3;
+		}
+		if (strchr("fhvwy", word[i]))
+		{
+			value = value + 4;
+		}
+		if (strchr("k", word[i]))
+		{
+			value = value + 5;
+		}
+		if (strchr("ix", word[i]))
+		{
+			value = value + 8;
+		}
+		if (strchr("qz", word[i]))
+		{
+			value = value + 10;
+		}
+	}
+	return value;
 }
+
+void Color(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+struct cell
+{
+	char character='0';
+	int colour=0;
+	int multAmmount=1;
+}cl;
+
+ifstream File;
+ifstream boardFile;
 
 int main() 
 {
-	int nr=0;
+	int nr=0,m=15;
 	string word;
+
+	cell* cl;
+	cl = new cell[m + 1];
 
 	typedef vector< tuple<string, int> > my_tuple;
 	my_tuple List0;
 
 	File.open("words.txt");
+	boardFile.open("BoardFile.txt");
+
+	for (int i = 1; i <= m; i++)
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			boardFile >> cl[i].colour;
+			boardFile >> cl[i].multAmmount;
+		}
+	}
+
+
+	for (int i = 1; i <= m; i++)
+	{
+		for (int i = 1; i <= m; i++)
+		{
+			int col = cl[i].colour;
+			Color(col);
+			cout << cl[i].character<<' ';
+		}
+		cout << endl;
+		Color(15);
+	}
+
+	/*for (int i = 1; i <= m ; i++)
+	{
+		for (int i = 1; i <= m*2; i++)
+		{
+			cout << cl[i].colour<<' ';
+			cout << cl[i].multAmmount<<"     ";
+		}
+		cout << endl;
+	}*/
 
 	while (File >> word)
 	{
 		if (check(word) == true)
 		{
-			List0.push_back(tuple<string, int>(word, 15));
+			List0.push_back(tuple<string, int>(word, value(word)));
 			nr++;
 		}
 	}
 
-	for (my_tuple::const_iterator i = List0.begin(); i != List0.end(); ++i) 
+	/*for (my_tuple::const_iterator i = List0.begin(); i != List0.end(); ++i) 
 	{
 		cout << get<0>(*i) <<' ';
 		cout << get<1>(*i) << endl;
-	}
+	}*/
 
 	cout << nr;
 
